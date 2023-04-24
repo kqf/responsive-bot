c67eba12ce4b97d0353a2b56babd7a0f668034f3
kqf <q.dev.null@gmail.com>
2023-04-24 08:11:48 +0100

    Share context between chats (#1)
    * Share context between the chats
    
    * Add no returns
    
    * Add no returns

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,9 @@
 import logging
-
+from functools import partial
 
 from telegram.ext import Application, MessageHandler, filters
 
 from bot.settings import config
-
 
 # Enable logging
 logging.basicConfig(
@@ -15,51 +14,52 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def _forward_to_admins(update, context):
+async def _forward_to_admins(update, context, data):
+    user_id = update.message.from_user.id
     for admin_id in config.admin_ids:
         message = await update.message.forward(admin_id)
         response = await context.bot.send_message(
             chat_id=admin_id,
             text="Please respond to this message to answer the question.",
         )
-        context.user_data[f"response_{admin_id}"] = {
+        data[f"response_{admin_id}"] = {
             "original_message_id": message.message_id,
             "message_id": response.message_id,
+            "user_id": user_id,
         }
 
-    return "Responded"
+
+async def thanks(update, context, data):
+    await _forward_to_admins(update, context, data)
 
 
-async def thanks(update, context):
-    await _forward_to_admins(update, context)
-
-
-async def handle_admin_reply(update, context):
-    print("Handling this")
+async def handle_admin_reply(update, context, data):
     message_id = update.message.reply_to_message.message_id
     for admin_id in config.admin_ids:
-        print(context.user_data)
-        response = context.user_data.get(f"response_{admin_id}")
-        # print(response)
+        response = data.get(f"response_{admin_id}")
         if response is None:
             return
 
         if response["message_id"] != message_id:
             continue
-        original_message_id = context.user_data[f"response_{admin_id}"][
-            "original_message_id"
-        ]
+
         resp = f"Admin replied: {update.message.text}"
-        await context.bot.send_message(chat_id=original_message_id, text=resp)
+        await context.bot.send_message(chat_id=response["user_id"], text=resp)
+        del data["response_{admin_id}"]
 
 
 def main():
     app = Application.builder().token(config.token).build()
-    app.add_handler(MessageHandler(~filters.COMMAND & ~filters.REPLY, thanks))
+    data = {}
+    app.add_handler(
+        MessageHandler(
+            ~filters.COMMAND & ~filters.REPLY, partial(thanks, data=data)
+        )
+    )
     app.add_handler(
         MessageHandler(
             filters.REPLY,
-            handle_admin_reply,
+            partial(handle_admin_reply, data=data),
         ),
     )
     app.run_polling()
