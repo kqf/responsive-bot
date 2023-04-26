fe1149489a3d8992e9b6f919fcc7358dfbe85f88
kqf <q.dev.null@gmail.com>
2023-04-26 07:08:04 +0100

    Remove everything (#5)
    * Remove the settings
    
    * Fix the bot settings

--- bot/settings.py
+++ bot/settings.py
@@ -5,8 +5,8 @@ class Config:
     def __init__(self):
         env = Env()
         env.read_env()
-        self.token = env("TOKEN")
-        self.admin_ids = env.list("ADMIN_IDS")
+        self.token = env("TOKEN", None)
+        self.admin_ids = env.list("ADMIN_IDS", None)
 
         self.port = env.int("PORT", 8443)
         self.webhook = env.str("WEBHOOK_URL", None)
