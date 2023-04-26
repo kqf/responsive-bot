0e263bf60ac68939403da1f5f4edeb2f08a38145
kqf <q.dev.null@gmail.com>
2023-04-26 07:18:56 +0100

    Remove the sqlnumpy (#7)
    * Remove the settings
    
    * Fix the bot settings
    
    * Add the dependency
    
    * Add the pre-commit hooks
    
    * Fix the test config
    
    * Remove the sqlnumpy

--- bot/settings.py
+++ bot/settings.py
@@ -6,7 +6,7 @@ class Config:
         env = Env()
         env.read_env()
         self.token = env("TOKEN", None)
-        self.admin_ids = env.list("ADMIN_IDS", None)
+        self.admin_ids = env.list("ADMIN_IDS", [])
 
         self.port = env.int("PORT", 8443)
         self.webhook = env.str("WEBHOOK_URL", None)
